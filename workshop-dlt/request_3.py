b7c991eba48930fe31f3b07cfbdb1a2beee051ff
Piotr Owczarek <piotrek.zwd@gmail.com>
2025-02-20 20:30:57 +0100

    dlt workshop

---
+++ workshop-dlt/request_3.py
@@ -0,0 +1,22 @@
+import requests
+
+BASE_API_URL = "https://us-central1-dlthub-analytics.cloudfunctions.net/data_engineering_zoomcamp_api"
+
+def paginated_getter():
+    page_number = 1
+    while True:
+        params = {'page': page_number}
+        response = requests.get(BASE_API_URL, params=params)
+        response.raise_for_status()
+        page_json = response.json()
+        print(f'Got page {page_number} with {len(page_json)} records')
+
+        if page_json:
+            yield page_json
+            page_number += 1
+        else:
+            break
+
+
+for page_data in paginated_getter():
+    print(page_data)
